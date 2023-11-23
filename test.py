acbe0e301110f4fea89f44dd64532ba9215eb75d
Sunho-97 <cow528@naver.com>
2023-11-23 11:17:21 +0900

    revise 2

--- test.py
+++ test.py
@@ -21,15 +21,11 @@ axes[0, 1].set_title('Correlation Analysis')
 for i, color in enumerate(['blue', 'green']):
     sns.histplot(data=data[:, i], bins=15, color=color, alpha=0.7, ax=axes[1, 0], label=f'Variable {i+1}')
 axes[1, 0].legend()
-axes[1, 0].set_xlabel('Values')
-axes[1, 0].set_ylabel('Frequency')
-axes[1, 0].set_title('Histogram of Variables')
+axes[1, 0].set(xlabel='Values', ylabel='Frequency', title='Histogram of Variables')
 
 # Scatter Plot
 sns.scatterplot(x=data[:, 0], y=data[:, 1], alpha=0.7, ax=axes[1, 1])
-axes[1, 1].set_xlabel('Variable 1')
-axes[1, 1].set_ylabel('Variable 2')
-axes[1, 1].set_title('Scatter Plot of Variable 1 vs Variable 2')
+axes[1, 1].set(xlabel='Variable 1', ylabel='Variable 2', title='Scatter Plot of Variable 1 vs Variable 2')
 
 plt.tight_layout()
 plt.show()
